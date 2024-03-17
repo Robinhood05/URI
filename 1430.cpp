cdc760658ab6a06f6f4c6018c7c3b8aca6ecb0cd
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-03-17 12:28:15 +0600

    Update 1430.cpp

--- 1430.cpp
+++ 1430.cpp
@@ -42,3 +42,78 @@ int main()
 	system ("pause");
 	return 0;
 }
+	#why my code showing wrong answer
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+ using namespace std;
+
+ 
+int
+main ()
+{
+  
+map < char, float >mp;
+  
+mp.insert (pair < char, float >('W', 1));
+  
+mp.insert (pair < char, float >('H', (float) 1 / 2));
+  
+mp.insert (pair < char, float >('Q', (float) 1 / 4));
+  
+mp.insert (pair < char, float >('E', (float) 1 / 8));
+  
+mp.insert (pair < char, float >('S', (float) 1 / 16));
+  
+mp.insert (pair < char, float >('T', (float) 1 / 32));
+  
+mp.insert (pair < char, float >('X', (float) 1 / 64));
+  
+ 
+string s;
+  
+while (cin >> s && s != "*")
+	{
+	  
+int sum = 0;
+	  
+int counts = 0;
+	  
+for (int i = 0; i < s.size (); i++)
+		{
+		  
+if (s[i] != '/')
+			{
+			  
+sum += mp[s[i]];
+			
+}
+		  else
+			{
+			  
+if (sum == 1)
+				{
+				  
+counts++;
+sum = 0;
+				
+}
+			  
+		
+			}
+		
+}
+
+		
+
+	  
+cout << counts << endl;
+	
+}
+  
+system ("pause");
+  
+return 0;
+
+}
